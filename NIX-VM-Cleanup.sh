1a38d1200991d1dfa14b047d73166a5eebd7d70c
Matt Kelly <matt.kelly@simspace.com>
2025-01-31 10:24:12 -0600

    Update NIX-VM-Cleanup.sh

--- NIX-VM-Cleanup.sh
+++ NIX-VM-Cleanup.sh
@@ -1,17 +1,12 @@
 find /var/log -type f -exec sh -c "cat /dev/null > {}" \;
-
 rm -rf /root/.cache
-
 rm -rf /tmp/*
-
 rm -rf /root/.wget-hsts
+find / -type f -name '.bash_history'
+find /var/log -type f -name '*.gz' -delete
 
 # Clear out history. Works with bash, but not with zsh.
-cat /dev/null | tee /root/.bash_history /home/simspace/.bash_history && history -c && init 0
+cat /dev/null | tee /root/.bash_history /home/simspace/.bash_history
 
-#Uncomment this to just remove the history session
-#exec rm "$HISTFILE" 
-
-find / -type f -name '.bash_history'
-
-find /var/log -type f -name '*.gz' -delete
+# Just remove the history session
+exec rm "$HISTFILE" 
